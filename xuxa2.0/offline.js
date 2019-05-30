3b50a9c279230f694cbb26a97e70d14178ac1017
fabiowerty <50372349+fabiowerty@users.noreply.github.com>
2019-05-30 14:41:37 -0300

    Add files via upload

---
+++ xuxa2.0/offline.js
@@ -0,0 +1,27 @@
+﻿{
+	"version": 1559237993,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/amigaa-sheet0.png",
+		"images/bala-sheet0.png",
+		"images/balacontrole-sheet0.png",
+		"images/pábaixa-sheet0.png",
+		"images/pácima-sheet0.png",
+		"images/páesquerda-sheet0.png",
+		"images/pádireita-sheet0.png",
+		"images/inimigaa-sheet0.png",
+		"images/distroii-sheet0.png",
+		"images/tiroinimigo-sheet0.png",
+		"images/tiledbackground.png",
+		"images/feijoada-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
